f3e03f3fc8a27ad680167300ab5c20ec3ee723fe
Gerardufoin <gerardufoin2@hotmail.com>
2023-11-19 21:35:52 +0100

    Update new webpack to output both ESM and UMD format

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -2,27 +2,42 @@ import path from 'path';
 import terserPlugin from 'terser-webpack-plugin';
 import { fileURLToPath } from 'url';
 
-export default {
+const config = {
 	entry: './src/index.js',
 	mode: 'production',
-	output: {
-		filename: 'dinorpg-animations.min.js',
-		path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist'),
-		library: 'DinoAnim',
-		libraryTarget: 'module',
-		module: true,
-		library: {
-			type: 'module'
-		}
-	},
 	optimization: {
 		minimize: true,
 		minimizer: [new terserPlugin({ extractComments: false })]
 	},
 	performance: {
 		hints: false
+	}
+};
+
+const standalone_config = {
+	...config,
+	output: {
+		filename: 'dinorpg-animations-test.min.js',
+		path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist'),
+		library: {
+			name: 'DinoAnim',
+			type: 'umd'
+		}
+	}
+};
+
+const esm_config = {
+	...config,
+	output: {
+		filename: 'dinorpg-animations.min.js',
+		path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist'),
+		library: {
+			type: 'module'
+		}
 	},
 	experiments: {
 		outputModule: true
 	}
 };
+
+export default [standalone_config, esm_config];
